b5bdffca4e374784d9d53e666fdbaf4a7eed3e52
Passifi <Pascal.wigger@gmx.de>
2026-06-05 11:28:13 +0200

    Working on animation

--- animation.asm
+++ animation.asm
@@ -1,6 +1,11 @@
 AnimationTimers equ $ff6000
 AnimationTimersEnd equ $ff6200
 SpriteArray equ $ff0000
+; non-player animations with preloaded spritedata 
+; when constructing the sprite table we use SpriteOffset to be added to the spritebase 
+; e.g. TileNo 123 is the base then we add the offset to that base 
+; when the Timer has run out 
+TimerDone equ 7
 handleAnimation:
   lea AnimationTimers,a0
   lea SpriteArray,a1 
@@ -9,8 +14,11 @@ handleAnimation:
   subq.l #1,d4
 .loop 
   move.b (TimerFlags,a0,d5),d0
-  btst #7,d0
+  btst #TimerDone,d0
   bne .next 
+  ; reset Time
+  move.b #0,(TimerFlags,a0,d5)
+  move.w (TimerIntervalStart,a0,d5),(TimerInterval,a0,d5)
   move.b (SpriteOffset,a1,d5),d0 
   move.b (SpriteOffsetMax,a1,d5),d1 
   cmp.b d0,d1
@@ -23,3 +31,17 @@ handleAnimation:
 .next
   dbf d4,.loop
   rts 
+; check the state
+playerAnimation:
+  lea (Timer+PlayerTimerOffset),a0  
+  lea PlayerAnimationAddress,a1 
+  move.b (TimerFlags,a0,d5),d0
+  btst #TimerDone,d0
+  move.b #0,(TimerFlags,a0,d5)
+  move.w (TimerIntervalStart,a0,d5),(TimerInterval,a0,d5)
+  
+  bne .next
+
+.next 
+  rts 
+
